ac65a7dffb0000a83f8fc28d2d322777a8d9f3d4
rk-0303 <rohitkharat9527@gmail.com>
2025-02-08 11:10:32 +0530

    Create Identity.java

---
+++ Operators/Identity.java
@@ -0,0 +1,13 @@
+class Identity
+	
+{
+  public static void main(Strg[] args)
+    {
+	System.out.pritnln("Hello World!");
+	System.out.pritnln("Name: Kharat Rohit");
+	System.out.pritnln("University Name: Shivaji University, Kolhapur");
+	System.out.pritnln("Branch: Computer Science and Technology.");
+	System.out.pritnln("Pursuing Course: Java Full stack development with Devops.");
+    }
+	
+}
